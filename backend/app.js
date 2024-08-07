7f770f8731590f7bc8cd8d721eacd6a4d0da5efa
rafavanzele <rafa_vanzele@hotmail.com>
2024-08-07 11:21:36 -0300

    add estruturas de serviços

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,11 @@ app.use(express.json())
 const conn = require('./db/conn')
 conn()
 
+
+// ROUTES
+const routes = require('./routes/router')
+app.use('/api', routes)
+
 app.listen(3000, function() {
     console.log('Servidor online!')
 })
---
+++ backend/routes/services.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+
+const serviceController = require('../controllers/serviceController')
+
+
+
+
+module.exports = router
