9b449e5428b2814eb37e5021514d9ae40f5e5589
rafavanzele <rafa_vanzele@hotmail.com>
2024-08-15 17:09:34 -0300

    trazendo serviços para a festa

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import partyFetch from '../axios/config'
+
+import { useState, useEffect } from 'react'
+
+import { useNavigate } from 'react-router-dom'
 
 const CreateParty = () => {
+  const [services, setServices] = useState([])
+
+  // LOAD SERVICE
+  useEffect(() => {
+    const loadServices = async() => {
+      const res = await partyFetch.get('/services')
+
+      setServices(res.data)
+    }
+
+    loadServices()
+  }, [])
+
+
   return (
     <div className='form-page'>
       <h2>Crie sua próxima Festa!</h2>
@@ -36,7 +54,22 @@ const CreateParty = () => {
         <div>
           <h2>Escolha os serviços</h2>
           <div className="services-container">
-            <p>serviços</p>
+            {services.length === 0 && <p>Carregando...</p> } 
+
+            {services.length > 0 && services.map((service) =>(
+              <div className="service" key={service._id}>
+                <img src={service.image} alt={service.name} />
+
+                <p className='service-name'>{service.name}</p>
+
+                <p className="service-price">R${service.price}</p>
+
+                <div className="checkbox-container">
+                  <input type="checkbox" value={service._id}/>
+                  <p>Marque para solicitar</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
 
